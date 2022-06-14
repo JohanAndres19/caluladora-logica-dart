a217c361c22f737e7cb369dbc0dd57fda3ebc078
JohanAndres19 <johanandresa@hotmail.com>
2022-06-14 08:09:29 -0500

    test de la transformacion a postfija

--- test/aprendiendo_dart_test.dart
+++ test/aprendiendo_dart_test.dart
@@ -1,8 +1,19 @@
 import 'package:aprendiendo_dart/aprendiendo_dart.dart';
+import 'package:aprendiendo_dart/logica/ConvertirInfijaPos.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('Posfija_prueba  parentesis', () {
+    var calcular = Convertir();
+    calcular.expresion = "~ ( A v Q ) ^ B -> D <-> A";
+    calcular.convertirAlista();
+    expect(calcular.posFIja(), "AQv~B^D->A<->");
+  });
+
+  test('Posfija_prueba doble parentesis', () {
+    var calcular = Convertir();
+    calcular.expresion = "~ ( A v Q ) ^ B -> ( D <-> A )";
+    calcular.convertirAlista();
+    expect(calcular.posFIja(), "AQv~B^DA<->->");
   });
 }
